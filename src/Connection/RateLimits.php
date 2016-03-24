7d7e1fa08c0680aba5932a7823c4886203f7e525
Jan Žahourek <zahourek.jan@zahycz.net>
2016-03-24 15:23:07 +0100

    Rate Limits Class repair

--- src/Connection/RateLimits.php
+++ src/Connection/RateLimits.php
@@ -34,14 +34,26 @@
 			$this->rateLimitReset = $reset;
 		}
 
+		/**
+		 * Get Rate Limit
+		 * @return int
+		 */
 		public function getLimit() {
 			return $this->rateLimitLimit;
 		}
 
+		/**
+		 * Get Rate Remaining
+		 * @return int
+		 */
 		public function getRemaining() {
 			return $this->rateLimitRemaining;
 		}
 
+		/**
+		 * Get Rate Remaining
+		 * @return int
+		 */
 		public function getReset() {
 			return $this->rateLimitReset;
 		}
